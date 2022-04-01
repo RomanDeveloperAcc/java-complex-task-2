74bd7a1c26261e39e93a09805e7805315f6b9b14
RomanDeveloperAcc <roma04dan@gmail.com>
2022-04-01 16:57:53 +0300

    removed unused variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,19 @@
 import interfaces.DBService;
 import models.DBConnectionData;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import services.ConnectionManagementService;
 import services.MSSQLJDBCService;
-import services.MSSQLQueryService;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
 
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
         String fileName;
-        String jdbcUrl = null;
-        String dbName = null;
-        String dbLogin = null;
-        String dbPassword = null;
-
 //        if (args.length != 1) {
 //            logger.error("Wrong amount of parameters");
 //            System.exit(0);
 //        }
 
+        fileName = "db.properties";
         ConnectionManagementService connectionManagementService = new ConnectionManagementService();
-        DBConnectionData dbConnectionData = connectionManagementService.getDBConnectionData("db.properties");
+        DBConnectionData dbConnectionData = connectionManagementService.getDBConnectionData(fileName);
 
 
         DBService mssqlQueryService = new MSSQLJDBCService(dbConnectionData);
